02936f69d66c127d055c0401278faf032a9c44bd
Antonio Kaplan <akaplan@ripple.com>
2019-12-09 12:56:36 -0800

    create express endpoints

--- server/app.js
+++ server/app.js
@@ -1,74 +1,75 @@
+const AqfrNetworkClient = require('aqfr.network.client');
 const RippleAPI = require('ripple-lib').RippleAPI;
+var express = require("express");
+var bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.listen(port, async () => {
+  await api.connect();
+  console.log("Server running on port:", port);
+
+});
+
+
 
 const api = new RippleAPI({
   // server: 'wss://s1.ripple.com' // Public rippled server
   server: 'wss://s.altnet.rippletest.net:51233' // Test Net
 });
 
-const myAddress = 'rBfLmb4EVdPANsc7X7oNtbydYqxJHphTm3';
-const secret = 'snSVD3hwfGvZTV51JFKD4VLczmXdj';
 
-const destinationAddress = 'rHJyNja4hmBVt8JfjyreP53pYyE9QiBNrx';
+async function start(myAddress, destinationAddress, secret, paymentData) {
 
-const paymentData = {
-  "TransactionType": "Payment",
-  "Account": myAddress,
-  "Amount": "10",
-  "Destination": destinationAddress,
-};
-
-
-async function start() {
-  await api.connect();
   console.log('getting account info for', myAddress);
   const accInfo = await api.getAccountInfo(myAddress);
 
+  console.log(accInfo);
+  console.log('getAccountInfo done');
 
-    console.log(accInfo);
-    console.log('getAccountInfo done');
+  const [maxLedgerVersion, txJSON] = await doPrepare(paymentData);
+  console.log('-------------------------------------------------------------')
+  console.log(txJSON)
 
-    const [maxLedgerVersion, txJSON] = await doPrepare(myAddress, destinationAddress);
-    console.log('-------------------------------------------------------------')
-    console.log(txJSON)
+  const [txID, signedTX] = await signTransaction (txJSON, secret)
+  console.log('-------------------------------------------------------------')
+  console.log('signedTX', signedTX)
+  console.log('-------------------------------------------------------------')
+  console.log('txID', txID)
 
-    const [txID, signedTX] = await signTransaction (txJSON, secret)
-    console.log('-------------------------------------------------------------')
-    console.log('signedTX', signedTX)
-    console.log('-------------------------------------------------------------')
-    console.log('txID', txID)
+  console.log('-------------------------------------------------------------')
+  const earliestLedgerVersion = await doSubmit(signedTX, secret)
+  console.log(earliestLedgerVersion)
 
-    console.log('-------------------------------------------------------------')
-    const earliestLedgerVersion = await doSubmit(signedTX, secret)
-    console.log(earliestLedgerVersion)
-
-    await watchLedger (maxLedgerVersion, txID, earliestLedgerVersion )
-
+  await watchLedger (maxLedgerVersion, txID, earliestLedgerVersion )
+  // const paymentResponse = await checkStatus (txID, earliestLedgerVersion)
+  return { txID, earliestLedgerVersion };
 }
 
 
 
 
 // Continuing after connecting to the API
-async function doPrepare(sender, destination) {
+async function doPrepare(payment) {
   // const sender = "rBfLmb4EVdPANsc7X7oNtbydYqxJHphTm3"
-  const preparedTx = await api.prepareTransaction({
-    "TransactionType": "Payment",
-    "Account": sender,
-    // "Amount": api.xrpToDrops("22"), // Same as "Amount": "22000000"
-    "Amount": "100",
-    "Destination": destination,
-  }, {
+  const preparedTx = await api.prepareTransaction( payment, {
     // Expire this transaction if it doesn't execute within ~5 minutes:
     "maxLedgerVersionOffset": 75
-  })
-  const maxLedgerVersion = preparedTx.instructions.maxLedgerVersion
+  });
+
+  const maxLedgerVersion = preparedTx.instructions.maxLedgerVersion;
   console.log("Prepared transaction instructions:", preparedTx.txJSON)
   console.log("Transaction cost:", preparedTx.instructions.fee, "XRP")
   console.log("Transaction expires after ledger:", maxLedgerVersion)
 
-
-
-  return [maxLedgerVersion, preparedTx.txJSON]
+  return [maxLedgerVersion, preparedTx.txJSON];
 }
 
 async function signTransaction (txJSON, secret) {
@@ -105,7 +106,6 @@ async function watchLedger (maxLedgerVersion, txID, earliestLedgerVersion ) {
     await checkStatus (txID, earliestLedgerVersion)
     if (ledger.ledgerVersion > maxLedgerVersion) {
       console.log("If the transaction hasn't succeeded by now, it's expired")
-
     }
   })
 }
@@ -120,10 +120,63 @@ async function checkStatus (txID, earliestLedgerVersion) {
     tx = await api.getTransaction(txID, {minLedgerVersion: earliestLedgerVersion})
     console.log("Transaction result:", tx.outcome.result)
     console.log("Balance changes:", JSON.stringify(tx.outcome.balanceChanges))
+    return tx.outcome;
   } catch(error) {
     console.log("Couldn't get transaction outcome:", error)
   }
 }
 
+app.get('/api/test/payment', async function (req, res) {
+  // const { sender, receiver, amount} = req.body;
+  const secret = 'snSVD3hwfGvZTV51JFKD4VLczmXdj';
 
-start()
+  const sender = 'rBfLmb4EVdPANsc7X7oNtbydYqxJHphTm3';
+  const receiver = 'rHJyNja4hmBVt8JfjyreP53pYyE9QiBNrx';
+  const amount = "20";
+
+  const paymentData = {
+    "TransactionType": "Payment",
+    "Account": sender,
+    "Amount": amount,
+    "Destination": receiver,
+  };
+
+  const paymentResponse = await start(sender, receiver, secret, paymentData);
+
+  console.log(paymentResponse);
+
+  res.json(paymentResponse);
+});
+
+app.get('/api/payment', async function (req, res) {
+  const { txID, earliestLedgerVersion} = req.query;
+  console.log(txID, earliestLedgerVersion)
+  // const paymentResponse = await watchLedger (maxLedgerVersion, txID, earliestLedgerVersion )
+  const paymentResponse = await checkStatus (txID, parseInt(earliestLedgerVersion, 10))
+
+  console.log(paymentResponse);
+
+  res.json(paymentResponse);
+});
+
+app.post('/api/payment', async function (req, res) {
+
+  const { sender, receiver, amount} = req.body;
+  const secret = 'snSVD3hwfGvZTV51JFKD4VLczmXdj';
+
+  // const myAddress = 'rBfLmb4EVdPANsc7X7oNtbydYqxJHphTm3';
+  // const destinationAddress = 'rHJyNja4hmBVt8JfjyreP53pYyE9QiBNrx';
+
+  const paymentData = {
+    "TransactionType": "Payment",
+    "Account": sender,
+    "Amount": amount,
+    "Destination": receiver,
+  };
+
+  const paymentResponse = await start(sender, receiver, secret, paymentData);
+
+  console.log(paymentResponse);
+
+  res.json(paymentResponse);
+});
